21b595c04d071c3824d1c5424f5ac6d90d599fdf
liluyao <1216969907@qq.com>
2019-05-25 23:34:58 +0800

    Merge branch 'master' of C:\Users\Administrator\Desktop\homework_HL_VR with conflicts.

---
+++ app/src/main/java/com/sunfusheng/vr/model/User.java
@@ -0,0 +1,23 @@
+package com.sunfusheng.vr.model;
+
+public class User {
+    int UId;
+    String UUsername;
+    String UPwd;
+
+    public int getUId() {
+        return UId;
+    }
+
+    public void setUId(int UId) {
+        this.UId = UId;
+    }
+
+    public String getUPwd() {
+        return UPwd;
+    }
+
+    public String getUUsername() {
+        return UUsername;
+    }
+}
