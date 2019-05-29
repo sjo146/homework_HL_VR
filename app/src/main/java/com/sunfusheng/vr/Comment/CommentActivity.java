7a917c77ab064e8d83e9cdc3280abbea1195d4b2
铿然 <thd0106@163.com>
2019-05-29 16:41:18 +0800

    评论页面做好

---
+++ app/src/main/java/com/sunfusheng/vr/Comment/CommentActivity.java
@@ -0,0 +1,14 @@
+package com.sunfusheng.vr.Comment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.sunfusheng.vr.R;
+
+public class CommentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_comment);
+    }
+}
