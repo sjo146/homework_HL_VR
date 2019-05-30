2c80dc129b9260bff99a21e1bab5baead054abff
liluyao <1216969907@qq.com>
2019-05-30 10:28:52 +0800

    Merge branch 'master' of C:\Users\Administrator\Desktop\homework_HL_VR with conflicts.

---
+++ app/src/main/java/com/sunfusheng/vr/MyInfo/MyInfoActivity.java
@@ -0,0 +1,13 @@
+package com.sunfusheng.vr.MyInfo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.sunfusheng.vr.R;
+
+public class MyInfoActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_info);
+    }
+}
